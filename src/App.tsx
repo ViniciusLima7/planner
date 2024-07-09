eabb28b5bfb4fd2743b19925ad814abc207315ff
Vinicius Lima <vinicius.lima_iasd@hotmail.com>
2024-07-08 21:40:58 -0300

    feat: completed homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
+import { ArrowRight, Calendar, MapPin } from "lucide-react";
+
 function App() {
   return (
-    <div className=" h-screen flex items-center justify-center">
+    <div className=" h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full pl-6 text-center space-y-10">
-        <p className="text-zinc-300 text-lg">
-          Convide seus amigos e planeje sua próxima viagem!
-        </p>
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape">
-          <input
-            type="text"
-            name=""
-            placeholder="You are where to go?"
-            id=""
-            className="bg-transparent text-lg placeholder-zinc-400"
-          />
-          <input
-            type="text"
-            name=""
-            placeholder="When?"
-            id=""
-            className="bg-transparent text-lg placeholder-zinc-400"
-          />
-          <button>More</button>
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo.svg" alt="Planner" />
+          <p className="text-zinc-300 text-lg">
+            Convide seus amigos e planeje sua próxima viagem!
+          </p>
+        </div>
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+          <div className="flex items-center gap-2 flex-1">
+            <MapPin className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              name=""
+              placeholder="You are where to go?"
+              id=""
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              name=""
+              placeholder="When?"
+              id=""
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none  w-40"
+            />
+          </div>
+          <div className="w-px h-6 bg-zinc-800"></div>
+          <button className="bg-lime-300 text-lime-950 rounded-xl px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            Continuar
+            <ArrowRight className="size-5 text-lime-950" />
+          </button>
         </div>
         <p className="text-sm  text-zinc-50">
           Ao planejar sua viagem pela plann.er você automaticamente concorda{" "}
