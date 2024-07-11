d972b92e203b735de9a3a59bb813dabf96734ee1
Vinicius <vinicius.lima_iasd@hotmail.com>
2024-07-10 23:00:05 -0300

    feat: add section two part 1

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,11 @@
-import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
+import {
+  Calendar,
+  CircleCheck,
+  Link2,
+  MapPin,
+  Plus,
+  Settings2,
+} from "lucide-react";
 
 export default function TripDetailsPage() {
   return (
@@ -70,7 +77,48 @@ export default function TripDetailsPage() {
             </div>
           </div>
         </div>
-        <div className="w-80"></div>
+        <div className="w-80 space-y-6">
+          <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Important links</h2>
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">
+                    Reserva Airbainb
+                  </span>
+                  <a
+                    href="http://localhost:5173/trips/123"
+                    className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                  >
+                    http://localhost:5173/trips/123fffffffffffffffssssssssssssfffffffffff
+                  </a>
+                </div>
+                <Link2 className="text-zinc-400 size-5 shrink-0" />
+              </div>
+
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">
+                    Reserva Futebol
+                  </span>
+                  <a
+                    href="http://localhost:5173/trips/123"
+                    className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                  >
+                    http://localhost:5173/trips/123fffffffffffffffssssssssssssfffffffffff
+                  </a>
+                </div>
+                <Link2 className="text-zinc-400 size-5 shrink-0" />
+              </div>
+            </div>
+            <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-xl px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
+              <Plus className="size-5" />
+              Register new link
+            </button>
+          </div>
+          <div className="w-full h-px bg-zinc-800" />
+          <div></div>
+        </div>
       </main>
     </div>
   );
