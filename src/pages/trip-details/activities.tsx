0acdc6f221fdfd91027fed69b4fa30cbaddd0f3b
Vinicius <vinicius.lima_iasd@hotmail.com>
2024-07-11 11:21:34 -0300

    fix: padding in activity

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -41,7 +41,7 @@ export default function Activities() {
               <div>
                 {category.activities.map((activity) => {
                   return (
-                    <div key={activity.id} className="space-y-2.5">
+                    <div key={activity.id} className="space-y-2.5 py-1">
                       <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
                         <CircleCheck className="size-5 text-lime-300" />
                         <span className="text-zinc-100">{activity.title}</span>
